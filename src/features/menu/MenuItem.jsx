0cfd48dc8a9b39daa4b9ae3bfde87988e9c6ddb3
Sahil Mandre <sahilmandre@gmail.com>
2024-02-26 23:07:35 +0530

    1st commit of React-Pizza app

---
+++ src/features/menu/MenuItem.jsx
@@ -0,0 +1,54 @@
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button";
+import { formatCurrency } from "../../utils/helpers";
+import { addItem } from "../cart/cartSlice";
+
+function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
+
+  const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+  }
+
+  return (
+    <li className="flex items-center gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`h-24 ${soldOut ? "opacity-50 grayscale" : ""}`}
+      />
+      <div className="flex grow flex-col self-stretch ">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex items-center justify-between">
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500 ">
+              Sold out
+            </p>
+          )}
+
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to Cart
+            </Button>
+          )}
+        </div>
+      </div>
+    </li>
+  );
+}
+
+export default MenuItem;
